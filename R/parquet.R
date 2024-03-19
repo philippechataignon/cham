0cff23286e7fdfb6e5203d64e0c3bf0e4aed4f29
Username <user@example.com>
2024-03-19 13:19:46 +0000

    Ajoute param quiet à write_parquet

--- R/parquet.R
+++ R/parquet.R
@@ -1,10 +1,18 @@
 #' Compression zstd pour write_parquet
 #' @export
-write_parquet <- function(dt, ...)
+write_parquet <- function(dt, path, quiet=F,...)
 {
   if (is.data.table(dt)) {
     dt = setDF(copy(dt))
   }
-  arrow::write_parquet(dt, ..., compression = "zstd")
+  if (!quiet) {
+    if ("SubTreeFileSystem" %in% class(path)) {
+      spath = path$base_path
+    } else {
+      spath = path
+    }
+    cat("Write s3", spath, "\n")
+  }
+  arrow::write_parquet(dt, path, ..., compression = "zstd")
 }
 
