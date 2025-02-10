a33de34b39345cffece724f27fbb85d1cd05fd3c
Username <user@example.com>
2025-02-10 14:08:07 +0000

    Corrige filename si filename est renseigné

--- R/write_duckdb_parquet.R
+++ R/write_duckdb_parquet.R
@@ -5,12 +5,21 @@
 #' @param dir : répertoire de sortie, éventuellement s3
 #' @param partition : chaine de la forme "var1, var2" pour créer une partition, pas de partition par défaut
 #' @param verbose : indique la requete SQL générée
-#' @return filename
+#' @return nom du fichier/répertoire
+#' @examples
+#' s3expl = "s3://insee/sern-div-exploitations-statistiques-rp/ear_rp"
+#' # Fichier simple ear_fam.parquet
+#' outfile = write_duckdb_parquet(conn = conn, name="ear_fam", dir = s3expl)
+#' # Fichier partitionné dans répertoire test_fam
+#' outfile = write_duckdb_parquet(conn, name="ear_fam",  dir = s3expl,
+#'   filename="test_fam", partition="c_annee_col", verbose=T)
 #' @export
 write_duckdb_parquet <- function(conn, name = NULL, filename = NULL, dir=".", partition = NULL, verbose = FALSE)
 {
   if (is.null(filename)) {
     filename = file.path(dir, paste0(name, ".parquet"))
+  } else {
+    filename = file.path(dir, filename)
   }
   if (is.null(partition)) {
     partition_str = ""
