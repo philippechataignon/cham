3b46b591734b29bed4b072bd8ae30706735f6b37
Philippe Chataignon <philippe.chataignon@insee.fr>
2025-04-28 09:56:38 +0000

    write_duckdb_parquet utilise directement la table duckdb

--- R/write_duckdb_parquet.R
+++ R/write_duckdb_parquet.R
@@ -1,7 +1,6 @@
 #' Écrit fichier parquet depuis une table duckdb
-#' @param conn : connexion duckdb
-#' @param name : nom de la table
-#' @param filename : nom du fichier ou répertoire si partition est renseigné, par défaut `name`
+#' @param table : table duckdb
+#' @param path : nom du fichier ou répertoire si partition est renseigné, par défaut le nom de la table
 #' @param dir : répertoire de sortie, éventuellement s3
 #' @param order_by : ordre éventuel de sortie sous forme de vecteur character
 #' @param partition : chaine de la forme "var1, var2" pour créer une partition, pas de partition par défaut
@@ -10,26 +9,26 @@
 #' @examples
 #' s3expl = "s3://insee/sern-div-exploitations-statistiques-rp/ear_rp"
 #' # Fichier simple ear_fam.parquet dasn répertoire dir sous s3
-#' outfile = write_duckdb_parquet(conn = conn, name="ear_fam", dir = s3expl)
+#' outfile = write_duckdb_parquet(ear_fam, dir = s3expl)
 #' # Fichier simple ear_fam.parquet trié dans répertoire courant
-#' outfile = write_duckdb_parquet(conn = conn, name="ear_fam", order_by=c("var1", "var2"))
+#' outfile = write_duckdb_parquet(ear_fam, order_by=c("var1", "var2"))
 #' # Fichier partitionné dans répertoire test_fam
-#' outfile = write_duckdb_parquet(conn, name="ear_fam",  dir = s3expl,
+#' outfile = write_duckdb_parquet(ear_fam,  dir = s3expl,
 #'   filename="test_fam", partition="c_annee_col", verbose=T)
 #' @export
+
 write_duckdb_parquet <- function(
-  conn,
-  name = NULL,
-  filename = NULL,
+  table,
+  path = NULL,
   dir = ".",
   partition = NULL,
   order_by = NULL,
   verbose = FALSE
 ) {
-  if (is.null(filename)) {
-    filename = file.path(dir, paste0(name, ".parquet"))
+  if (is.null(path)) {
+    path = file.path(dir, paste0(deparse(substitute(table)), ".parquet"))
   } else {
-    filename = file.path(dir, filename)
+    path = file.path(dir, path)
   }
   if (is.null(partition)) {
     partition_str = ""
@@ -43,12 +42,14 @@ write_duckdb_parquet <- function(
   } else {
     order_by = paste(" ORDER BY", paste0(order_by, collapse = ','))
   }
-
   cmd = glue::glue(
-    "COPY (FROM {name} {order_by}) TO '{filename}'
+    "COPY (FROM {table$lazy_query$x} {order_by}) TO '{path}'
     (FORMAT 'parquet', COMPRESSION 'zstd' {partition_str})"
   )
   if (verbose) cat(cmd, "\n")
-  dbExecute(conn, cmd)
-  filename
+  dbExecute(
+    table$src$con,
+    cmd
+  )
+  path
 }
