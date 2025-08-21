a87ecf7ebba920b3b647f642fe08642da0809e59
Philippe Chataignon <philippe.chataignon@gmail.com>
2025-08-21 08:26:11 +0000

    Ajoute fonctions geoparquet

---
+++ R/geoparquet.R
@@ -0,0 +1,125 @@
+#' Permet de transformer une table duckdb en sf
+#' @export
+tbl2sf <- function(
+    table,
+    crs
+)
+{
+    table |>
+      arrow::to_arrow() |>
+      sf::st_as_sf(crs = crs) |>
+      as_tibble() |>
+      sf::st_as_sf(crs = crs)
+}
+
+#' Permet de lire un fichier geoparquet en sf
+#' @export
+read_geoparquet <- function(file, crs=NULL, method = c("duckarrow", "st_read", "geoarrow"), convert = F, verbose=F)
+{
+  method = match.arg(method)
+  is_s3 = grepl("^s3://", file)
+  if (!is_s3 & !file.exists(file))
+    stop("Le fichier ", file, " n'existe pas")
+  if (method == "geoarrow") {
+    if (convert | is_s3)
+      stop("Pas de conversion possible avec la méthode 'geoarrow'")
+    require(geoarrow)
+    if (is.null(crs))
+      crs = "OGC:CRS84"
+    open_dataset(file) |>
+      sf::st_as_sf(crs=crs) |>
+      as_tibble() |>
+      sf::st_as_sf(crs=crs) -> ret
+  } else {
+    conn = get_conn()
+    meta = DBI::dbGetQuery(
+      conn,
+      paste0("
+      SELECT decode(value)
+      FROM parquet_kv_metadata('", file, "')
+      WHERE key='geo'
+      ")
+    )[1,]
+    if (verbose)
+      cat(jsonlite::prettify(meta), "\n")
+    meta = jsonlite::fromJSON(meta)
+    geom = meta$primary_column
+    crs_src = paste(
+      meta$columns$geometry$crs$id$authority,
+      meta$columns$geometry$crs$id$code,
+      sep=":"
+    )
+    if (length(crs_src) == 0)
+      crs_src = "OGC:CRS84"
+    if (is.null(crs))
+      crs_dest = crs_src
+    else if (is.numeric(crs))
+      crs_dest = paste0("EPSG:", crs)
+    else
+      crs_dest = crs
+    if (!convert) {
+      sql_geom = paste0("st_aswkb(", geom, ")")
+    } else {
+      sql_geom = paste0("st_aswkb(st_transform(", geom, ", '", crs_src, "', '", crs_dest, "',  xy := true))")
+    }
+    q = paste0(
+      "SELECT * REPLACE (", sql_geom, " as geometry)
+      FROM read_parquet('", file, "')"
+    )
+    if (verbose)
+      cat(q, "\n")
+    if (method == "st_read") {
+      ret = sf::st_read(conn, query = q, geometry_column = "geometry", as_tibble=T) |>
+        sf::st_set_crs(crs_dest)
+    } else if (method == "duckarrow") {
+      ret = tbl(conn, sql(q)) |>
+        tbl2sf(crs=crs_dest)
+    }
+  }
+  ret
+}
+
+#' Permet d'écrire un fichier geoparquet en sf
+#' @export
+write_geoparquet <- function (sf, dsn, verbose=F)
+{
+  if (!inherits(sf, "sf")) {
+    stop("Must be sf data format")
+  }
+  if (missing(dsn)) {
+    stop("Missing output file")
+  }
+  sf::st_geometry(sf) = "geometry"
+  geom_col = attr(sf, "sf_column")
+  col_meta <- list()
+  col_meta[[geom_col]] <- list(
+    crs = jsonlite::fromJSON(sf::st_as_text(sf::st_crs(sf[[geom_col]]), projjson=T)),
+    encoding = "WKB",
+    geometry_types = list()
+  )
+  geo_metadata <- list(
+    version = "1.1.0",
+    primary_column = geom_col,
+    columns = col_meta
+  )
+  geo_metadata = jsonlite::toJSON(geo_metadata, auto_unbox=T)
+  if (verbose)
+    cat(jsonlite::prettify(geo_metadata), "\n")
+  df <- as.data.frame(sf)
+  obj_geo <- sf::st_as_binary(df[[geom_col]])
+  attr(obj_geo, "class") <- c("arrow_binary", "vctrs_vctr", attr(obj_geo, "class"), "list")
+  df[[geom_col]] <- obj_geo
+  tbl <- arrow::Table$create(df)
+  tbl$metadata[["geo"]] <- geo_metadata
+  arrow::write_parquet(tbl, sink = dsn, compression = "zstd")
+  invisible(sf)
+}
+
+#' Récupère le polygone associé à un hex_id
+#' @export
+get_h3map <- function(hex_id)
+{
+  rbindlist(cellToBoundary(hex_id), idcol="id") |>
+    sf::sf_polygon("lat", "lng", polygon_id="id") |>
+    sf::st_sf(crs=4326)
+}
--- create_source.sh
+++ create_source.sh
@@ -2,4 +2,5 @@
 PKG=$(Rscript -e "sink('/dev/null');devtools::document(roclets = c('rd', 'collate', 'namespace')); file=devtools::build(quiet=T); sink(); cat(file, '\n')")
 echo $PKG
 # echo $(basename $PKG)
-curl --upload-file ${PKG} "https://nexus.insee.fr/repository/r-local/src/contrib/$(basename ${PKG})"
+curl -v -u "vgkwl1:$NEXUS_PW" --upload-file ${PKG} "https://nexus.insee.fr/repository/r-local/src/contrib/$(basename ${PKG})"
+# curl -v --upload-file ${PKG} "https://nexus.insee.fr/repository/r-local/src/contrib/$(basename ${PKG})"
