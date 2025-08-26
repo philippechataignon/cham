b25189ca5c5b0f77a02597a0042b27c804024ec3
Philippe Chataignon <philippe.chataignon@gmail.com>
2025-08-26 11:54:14 +0000

    Restructure get_rp gen et edl

--- R/rp.R
+++ R/rp.R
@@ -1,90 +1,38 @@
-rp_gen_files = list(
-  "2020" = list(
-    pind = c(
-      '{root}/A100020/GEN_A1000204_DIMETPARQUET/MET.parquet',
-      '{root}/A100020/GEN_A1000205_DIDOMPARQUET/DOM.parquet'
-    ),
-    plog = c(
-      '{root}/A100020/GEN_A1000204_DMMETPARQUET/MET.parquet',
-      '{root}/A100020/GEN_A1000205_DMDOMPARQUET/DOM.parquet'
-    ),
-    cind = c(
-      '{root}/A100020/GEN_A1000202_DIMETPARQUET/MET.parquet',
-      '{root}/A100020/GEN_A1000206_DIDOMPARQUET/DOM.parquet'
-    ),
-    clog = c(
-      '{root}/A100020/GEN_A1000202_DMMETPARQUET/MET.parquet',
-      '{root}/A100020/GEN_A1000206_DMDOMPARQUET/DOM.parquet'
-    ),
-    cfam = c(
-      '{root}/A100020/GEN_A1000202_DFMETPARQUET/MET.parquet',
-      '{root}/A100020/GEN_A1000206_DFDOMPARQUET/DOM.parquet'
+rp_gen_files = lapplyn(
+  2020:2022,
+  \(an) {
+    an = an %% 100L
+    list(
+      pind = c(
+        gen(glue::glue('A1000{an}/GEN_A1000{an}4_DIMETPARQUET/MET.parquet')),
+        gen(glue::glue('A1000{an}/GEN_A1000{an}5_DIDOMPARQUET/DOM.parquet'))
+      ),
+      plog = c(
+        gen(glue::glue('A1000{an}/GEN_A1000{an}4_DMMETPARQUET/MET.parquet')),
+        gen(glue::glue('A1000{an}/GEN_A1000{an}5_DMDOMPARQUET/DOM.parquet'))
+      ),
+      cind = c(
+        gen(glue::glue('A1000{an}/GEN_A1000{an}2_DIMETPARQUET/MET.parquet')),
+        gen(glue::glue('A1000{an}/GEN_A1000{an}6_DIDOMPARQUET/DOM.parquet'))
+      ),
+      clog = c(
+        gen(glue::glue('A1000{an}/GEN_A1000{an}2_DMMETPARQUET/MET.parquet')),
+        gen(glue::glue('A1000{an}/GEN_A1000{an}6_DMDOMPARQUET/DOM.parquet'))
+      ),
+      cfam = c(
+        gen(glue::glue('A1000{an}/GEN_A1000{an}2_DFMETPARQUET/MET.parquet')),
+        gen(glue::glue('A1000{an}/GEN_A1000{an}6_DFDOMPARQUET/DOM.parquet'))
+      )
     )
-  ),
-  "2021" = list(
-    pind = c(
-      '{root}/A100021/GEN_A1000214_DIMETAPARQUET/META.parquet',
-      '{root}/A100021/GEN_A1000214_DIMETBPARQUET/METB.parquet',
-      '{root}/A100021/GEN_A1000214_DIMETCPARQUET/METC.parquet',
-      '{root}/A100021/GEN_A1000215_DIDOMPARQUET/DOM.parquet'
-    ),
-    plog = c(
-      '{root}/A100021/GEN_A1000214_DMMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000215_DMDOMPARQUET/DOM.parquet'
-    ),
-    cind = c(
-      '{root}/A100021/GEN_A1000212_DIMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000216_DIDOMPARQUET/DOM.parquet'
-    ),
-    clog = c(
-      '{root}/A100021/GEN_A1000212_DMMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000216_DMDOMPARQUET/DOM.parquet'
-    ),
-    cfam = c(
-      '{root}/A100021/GEN_A1000212_DFMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000216_DFDOMPARQUET/DOM.parquet'
-    )
-  ),
-  "2022" = list(
-    pind = c(
-      '{root}/A100021/GEN_A1000224_DIMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000225_DIDOMPARQUET/DOM.parquet'
-    ),
-    plog = c(
-      '{root}/A100021/GEN_A1000224_DMMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000225_DMDOMPARQUET/DOM.parquet'
-    ),
-    cind = c(
-      '{root}/A100021/GEN_A1000222_DIMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000226_DIDOMPARQUET/DOM.parquet'
-    ),
-    clog = c(
-      '{root}/A100021/GEN_A1000222_DMMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000226_DMDOMPARQUET/DOM.parquet'
-    ),
-    cfam = c(
-      '{root}/A100021/GEN_A1000222_DFMETPARQUET/MET.parquet',
-      '{root}/A100021/GEN_A1000226_DFDOMPARQUET/DOM.parquet'
-    )
-  )
+  }
 )
-
-paths = list(
-  ls3 = list(
-    "gen_root" = "s3://mad/insee",
-    "edl_root" = "s3://insee/sern-div-exploitations-statistiques-rp/edl",
-    "ear_root" = "s3://insee/sern-div-exploitations-statistiques-rp/ear/{x}"
-  ),
-  aus = list(
-    "gen_root" = "W:/",
-    "edl_root" = "X:/HAB-Pole-EDL-BasesRP",
-    "ear_root" = "X:/HAB-MaD-SeRN/ear/{x}"
-  ),
-  pc = list(
-    "gen_root" = "~/work/insee/rp/an={an}/{x}/",
-    "ear_root" = "~/work/insee/ear/{x}"
+# correction pour pind du RP21 en 3 morceaux !
+rp_gen_files[["2021"]]$pind <- c(
+      gen('A100021/GEN_A1000214_DIMETAPARQUET/META.parquet'),
+      gen('A100021/GEN_A1000214_DIMETBPARQUET/METB.parquet'),
+      gen('A100021/GEN_A1000214_DIMETCPARQUET/METC.parquet'),
+      gen('A100021/GEN_A1000215_DIDOMPARQUET/DOM.parquet')
   )
-)
 
 #' Renvoie une liste des tables du RP21
 #' @param conn : connexion duckdb, peut être obtenu par la fonction get_conn
@@ -95,34 +43,26 @@ paths = list(
 #' trp21 <- get_rp21(conn)
 #' # population France
 #' trp21$pind |>
+#'   dplyr::filter(dc < "975") |>
 #'   dplyr::count(wt = ipondi)
 #' @export
-get_rp <- function(conn, an, src = c("gen", "edl", "dmtr"), verbose = FALSE) {
+get_rp <- function(conn, an, src = c("gen", "edl", "misc"), verbose = FALSE) {
   src = match.arg(src)
   if (src == "edl" && site == "pc") {
     src = "gen"
   }
   # gen ----
   if (src == "gen") {
-    root = paths[[site]]$gen_root
     if (site %in% c("ls3", "aus")) {
-      files = lapply(
-        rp_gen_files[[as.character(an)]],
-        function(x) gsub('{root}', root, x, fixed = T)
-      )
-    } else if (site == "pc") {
-      pat = gsub('{an}', an, root, fixed = T)
-      files = extend(rp_ext, pat)
+      files = rp_gen_files[[as.character(an)]]
     } else {
       stop("src non présente sur ce site")
     }
     # edl ----
   } else if (src == "edl") {
-    if (!site %in% c("ls3", "aus")) {
-      stop("src non présente sur ce site")
-    } else if (site == "ls3" && an == 2022) {
-      files = extend(rp_ext, file.path(s3expl, "edl/an=2022/{x}.parquet"))
-    } else {
+    if (site == "ls3") {
+      files = extend(rp_ext, file.path(s3expl, glue::glue("edl/an={an}/{{x}}.parquet")))
+    } else if (site == "aus") {
       an2 = an %% 100
       angeo2 = (an + 2) %% 100
       cvt = list(
@@ -132,16 +72,18 @@ get_rp <- function(conn, an, src = c("gen", "edl", "dmtr"), verbose = FALSE) {
         "clog" = "compl_log",
         "cfam" = "compl_fam"
       )
-      root = paths[[site]]$edl_root
+      root = "X:/HAB-Pole-EDL-BasesRP"
       files = extend(
         cvt[rp_ext],
         paste0(root, "/RP", an2, "/PARQUET/{x}", angeo2, "/")
       )
       names(files) = names(cvt)
+    } else {
+      stop("Source edl non présente sur ce site")
     }
-  } else if (src == "dmtr" && an == 2022) {
+  } else if (src == "misc" && an == 2022) {
     files = extend(rp_ext, file.path(s3perso, "edl/RP22/{x}.parquet"))
-  } else if (src == "dmtr" && an == 2021) {
+  } else if (src == "misc" && an == 2021) {
     files = extend(rp_ext, file.path(s3expl, "rp_repond/rprepond_{x}.parquet"))
   }
   ret = tbl_list(conn, files, lower = TRUE, verbose = verbose)
@@ -196,20 +138,3 @@ rp_ext = c("pind", "plog", "cind", "clog", "cfam")
 #' Extensions EAR
 #' @export
 ear_ext = c("ind", "log", "fam", "liens")
-
-#' Renvoie une liste des fichiers EAR
-#' @param conn : connexion duckdb, peut être obtenu par la fonction get_conn
-#' @return Liste de 4 éléments nommés 'ind', 'log', 'fam', 'liens' pour être passée
-#' à `get_tbl` ou `get_ds` selon exemple ci-dessous
-#' @examples
-#' conn <- get_conn()
-#' t_ear <- tbl_list(conn, ear_files(), level=1)
-#' t_ear$ind |>
-#'   dplyr::group_by(an) |>
-#'   dplyr::count() |>
-#'   dplyr::arrange(an) |>
-#'   print()
-#' @export
-ear_files <- function() {
-  extend(ear_ext, paths[[site]]$ear_root)
-}
