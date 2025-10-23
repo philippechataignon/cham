0e05460ba1c3f8f8d0212ce5363fdda0e13e4d3a
Philippe Chataignon <philippe.chataignon@gmail.com>
2025-10-23 17:16:19 +0200

    Utilise PASSWORDINSEE pour create_source

--- create_source.sh
+++ create_source.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PKG=$(Rscript -e "sink('/dev/null');devtools::document(roclets = c('rd', 'collate', 'namespace')); file=devtools::build(quiet=T); sink(); cat(file, '\n')")
 echo $PKG
-curl -u "vgkwl1:$NEXUS_PW" --upload-file ${PKG} "https://nexus.insee.fr/repository/r-local/src/contrib/$(basename ${PKG})"
+curl -u "vgkwl1:$PASSWORDINSEE" --upload-file ${PKG} "https://nexus.insee.fr/repository/r-local/src/contrib/$(basename ${PKG})"
